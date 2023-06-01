c080fb5963ce16d4a41d2d43981866f6855a0152
Danny <76977155+DNYFZR@users.noreply.github.com>
2023-06-01 16:52:26 +0100

    Updated get method & created tests

--- src/api.rs
+++ src/api.rs
@@ -1,9 +1,14 @@
-use reqwest::blocking::{get, Client, Response};
+use reqwest::blocking::{Client, Response};
 use reqwest::Error;
 use serde_json;
 
-pub fn get_json(url: &str) -> Result<serde_json::Value, reqwest::Error>{
-    let res: serde_json::Value = get(url)?.json()?;
+pub fn get_json(url: &str, header_key:&str, header_value:&str,) -> Result<serde_json::Value, reqwest::Error>{
+    let cli = Client::new();
+    let res: serde_json::Value = cli
+        .get(url)
+        .header(header_key, header_value)
+        .send()?
+        .json()?;
     
     Ok(res)
 }
@@ -40,5 +45,79 @@ pub fn delete_json(url: &str, header_key:&str, header_value:&str, body: String) 
         .send()?;
     
     Ok(res)
+}
+
+// Tests
+#[cfg(test)]
+mod test_api {
+    use super::*;
+
+    #[test]
+    fn test_get() {
+        let base_url = "https://dummyjson.com/products/";
+        let header_key = "Content-Type"; 
+        let header_value = "application/json";
+        let product_endpoint = "1";
+    
+        let url = format!("{}{}", base_url, product_endpoint);
+    
+        // Get reqest
+        let resp = get_json(&url, header_key, header_value);
+        
+        assert_eq!(resp.is_ok(), true)
+    }
+    
+    #[test]
+    fn test_post() {
+        // API params
+        let base_url = "https://dummyjson.com/products/";
+        let header_key = "Content-Type"; 
+        let header_value = "application/json";
+        let product_endpoint = "add";
+    
+        let url = format!("{}{}", base_url, product_endpoint);
+    
+        // Post request
+        let body:serde_json::Result<serde_json::Value> = serde_json::from_str("{\"title\": \"Iphone Galaxy + 1\"}");
+        let resp = post_json(&url, header_key, header_value, body.expect("JSON").to_string());
+    
+        assert_eq!(resp.is_ok(), true)
+    
+    }
+    
+    #[test]
+    fn test_put() {
+        // API params
+        let base_url = "https://dummyjson.com/products/";
+        let header_key = "Content-Type"; 
+        let header_value = "application/json";
+        let product_endpoint = "1";
+    
+        let url = format!("{}{}", base_url, product_endpoint);
+    
+        // Put request
+        let body:serde_json::Result<serde_json::Value>  = serde_json::from_str("{\"title\": \"iPhone Galaxy +1\"}");
+        let resp = put_json(&url, header_key, header_value, body.expect("JSON").to_string());
+    
+        assert_eq!(resp.is_ok(), true)
+    }
+    
+    #[test]
+    fn test_delete() {
+        // API params
+        let base_url = "https://dummyjson.com/products/";
+        let header_key = "Content-Type"; 
+        let header_value = "application/json";
+        let product_endpoint = "1";
+    
+        let url = format!("{}{}", base_url, product_endpoint);
+    
+        // Delete request
+        let body:serde_json::Result<serde_json::Value>  = serde_json::from_str("{\"title\": \"iPhone Galaxy +1\"}");
+        let resp = delete_json(&url, header_key, header_value, body.expect("JSON").to_string());
+    
+        assert_eq!(resp.is_ok(), true)
+    
+    }
 }
 
